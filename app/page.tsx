032ab2161de8ca3c87523cf972f20c4c2c8b90bb
dewanshpoddar <dewanshpoddar@gmail.com>
2026-04-06 06:53:14 +0530

    feat: remove ChatBot section from homepage in favor of dedicated AI Advisor page

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function HomePage() {
       <CalculatorSection />
       <IntentSection />
       <TestimonialsSection />
-      <ChatBot />
       <FinalCTA />
+
       <LeadForm />
     </>
   )
